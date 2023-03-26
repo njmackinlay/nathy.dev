2486aa967387130e79e6e3735e6719277ab9c0e0
nathy <njmackinlay@gmail.com>
2023-03-25 21:52:30 -0700

    Create package and set up prettier.

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className='App'>nathy.dev</div>;
+}
+
+export default App;
